bfe5c799fb448407e144fe90aeebb93f2873b109
PureWindSN <1983457078@qq.com>
2022-12-24 23:41:49 +0800

    add custom view

---
+++ WDUILib/Classes/WDEmptyTableView.h
@@ -0,0 +1,17 @@
+//
+//  WDEmptyTableView.h
+//  WDUILib_Example
+//
+//  Created by Wind on 2022/12/24.
+//  Copyright © 2022 PureWindSN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WDEmptyTableView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WDUILib/Classes/WDEmptyTableView.m
@@ -0,0 +1,21 @@
+//
+//  WDEmptyTableView.m
+//  WDUILib_Example
+//
+//  Created by Wind on 2022/12/24.
+//  Copyright © 2022 PureWindSN. All rights reserved.
+//
+
+#import "WDEmptyTableView.h"
+
+@implementation WDEmptyTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
